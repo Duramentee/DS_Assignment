6bdb01ce5fa6105302e5788cd28710a16e7ade78
Kojimamemes <lsx3193161830@163.com>
2024-10-25 12:08:31 +0800

    数据结构结课作业

---
+++ cmake-build-wsl-debug/CMakeFiles/ds.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ds.dir/LinkedList.cpp.o"
+  "CMakeFiles/ds.dir/LinkedList.cpp.o.d"
+  "ds"
+  "ds.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ds.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
